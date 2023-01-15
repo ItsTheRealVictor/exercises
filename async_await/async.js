58d5d30863b2e1cd56552c958ab4abe0d0258167
Victor Delgado <valexdelgado@gmail.com>
2023-01-14 18:24:00 -0800

    working on async/await exercises

--- async_await/async.js
+++ async_await/async.js
@@ -1,16 +1,39 @@
-const deck = {
-    async initNewDeck() {
-        let res = await axios.get('https://deckofcardsapi.com/api/deck/new/')
-        this.deck_id = res.data.deck_id
-    },
+// #1)
+const randomNumber = Math.floor(Math.random() * 100)
+const url = `http://numbersapi.com/${randomNumber}/?json`
 
-    async shuffle(){
-        let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deck_id}/shuffle/`)
-    },
-    
-    async drawCard(){
-        let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deck_id}/draw/?count=1`)
-        console.log(res)
-    }
-    
+async function getNum(){
+    let res = await axios.get(url)
+    console.log(res.data.text)
 }
+getNum()
+
+
+// #2)
+
+const numbers = [23, 56, 39, 64, 25, 66, 444]
+async function getNumbers() {
+    let res = await axios.get(`http://numbersapi.com/${numbers}/?json`)
+    console.log(res.data)
+}
+getNumbers()
+
+
+// #3)
+
+const favoriteNumber = 29
+async function getFacts(){
+    const facts = []
+    for (let i=0; i < 4; i++){
+        let res = await axios.get(`http://numbersapi.com/${favoriteNumber}/?json`)
+        facts.push(res.data.text)
+    }
+    let myNumsList = document.createElement('ul')
+    facts.forEach(r=> {
+        let myNum = document.createElement('li')
+                myNum.innerHTML = r
+                document.body.appendChild(myNumsList)
+                myNumsList.appendChild(myNum)
+            })
+}
+getFacts()
